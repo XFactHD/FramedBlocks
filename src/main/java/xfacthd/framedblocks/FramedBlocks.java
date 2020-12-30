8b3f2fb3549a5d2250fd120a3236ecc08708959f
XFactHD <xfacthd@gmx.de>
2020-12-31 00:11:40 +0100

    Sort ItemGroup

--- src/main/java/xfacthd/framedblocks/FramedBlocks.java
+++ src/main/java/xfacthd/framedblocks/FramedBlocks.java
@@ -1,27 +1,35 @@
 package xfacthd.framedblocks;
 
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import xfacthd.framedblocks.common.FBContent;
+import xfacthd.framedblocks.common.block.IFramedBlock;
 
 @Mod(FramedBlocks.MODID)
 public class FramedBlocks
 {
 	public static final String MODID = "framedblocks";
 	
-    private static final Logger LOGGER = LogManager.getLogger();
-
     public static final ItemGroup FRAMED_GROUP = new ItemGroup("framed_blocks")
     {
         @Override
         public ItemStack createIcon() { return new ItemStack(FBContent.blockFramedCube); }
-    };
 
-    public FramedBlocks()
-	{
-        
-    }
+        @Override
+        public void fill(NonNullList<ItemStack> items)
+        {
+            super.fill(items);
+            items.sort((s1, s2) ->
+            {
+                if (s1.getItem() == FBContent.itemFramedHammer) { return 1; }
+                if (s2.getItem() == FBContent.itemFramedHammer) { return -1; }
+
+                Block b1 = ((BlockItem) s1.getItem()).getBlock();
+                Block b2 = ((BlockItem) s2.getItem()).getBlock();
+                return ((IFramedBlock)b1).getBlockType().compareTo(((IFramedBlock)b2).getBlockType());
+            });
+        }
+    };
 }
