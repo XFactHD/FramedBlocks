c8bd373e930af4b23e7c8799b0d1a9e651f9d7d9
XFactHD <xfacthd@gmx.de>
2021-01-10 02:23:48 +0100

    Finish FramedInnerCornerSlopeModel

--- src/main/java/xfacthd/framedblocks/client/model/FramedInnerCornerSlopeModel.java
+++ src/main/java/xfacthd/framedblocks/client/model/FramedInnerCornerSlopeModel.java
@@ -62,6 +62,55 @@ public class FramedInnerCornerSlopeModel extends FramedBlockModel
                         createSlopeTriangle(finalQuad, pos, uv, light, dir, type)
                 ));
             }
+            else if (type.isHorizontal())
+            {
+                if (!ModelUtils.isFacingTowards(quad, dir.getOpposite()))
+                {
+                    BakedQuad finalQuad = quad;
+                    if ((type.isTop() && quad.getFace() == Direction.DOWN) || (!type.isTop() && quad.getFace() == Direction.UP))
+                    {
+                        quadIterator.remove();
+
+                        modifiedQuads.add(ModelUtils.modifyQuad(quad, (pos, color, uv, light, normal) ->
+                        {
+                            Direction fakeDir = type.isRight() ? dir.rotateY() : dir;
+                            FramedSlopeModel.createTopBottomTriangle(finalQuad, pos, uv, light, fakeDir);
+                        }));
+                    }
+                    else if ((type.isRight() && quad.getFace() == dir.rotateYCCW()) || (!type.isRight() && quad.getFace() == dir.rotateY()))
+                    {
+                        quadIterator.remove();
+
+                        modifiedQuads.add(ModelUtils.modifyQuad(quad, (pos, color, uv, light, normal) ->
+                                createSideTriangle(finalQuad, pos, uv, light, dir, type)
+                        ));
+                    }
+                }
+                else
+                {
+                    quadIterator.remove();
+
+                    Direction quadFace = quad.getFace();
+                    if (ModelUtils.isFacingTowards(quad, Direction.UP) || ModelUtils.isFacingTowards(quad, Direction.DOWN) &&
+                            dir == Direction.NORTH && !type.isRight())
+                    {
+                        quadFace = dir.getOpposite();
+                    }
+
+                    quad = new BakedQuad(
+                            quad.getVertexData(),
+                            quad.getTintIndex(),
+                            quadFace,
+                            quad.func_187508_a(),
+                            quad.shouldApplyDiffuseLighting()
+                    );
+
+                    BakedQuad finalQuad = quad;
+                    modifiedQuads.add(ModelUtils.modifyQuad(quad, (pos, color, uv, light, normal) ->
+                            createSlopeTriangle(finalQuad, pos, uv, light, dir, type)
+                    ));
+                }
+            }
         }
 
         builder.addAll(quads);
@@ -70,7 +119,7 @@ public class FramedInnerCornerSlopeModel extends FramedBlockModel
         return builder.build();
     }
 
-    public static void createSlopeTriangle(BakedQuad quad, float[][] pos, float[][] uv, float[][] light, Direction dir, CornerType type)
+    private static void createSlopeTriangle(BakedQuad quad, float[][] pos, float[][] uv, float[][] light, Direction dir, CornerType type)
     {
         //Needs special casing for some reason
         if (type == CornerType.TOP && quad.getFace() == dir.rotateYCCW())
@@ -96,7 +145,72 @@ public class FramedInnerCornerSlopeModel extends FramedBlockModel
         }
         else
         {
-            //TODO: implement
+            if (ModelUtils.isFacingTowards(quad, Direction.UP) || ModelUtils.isFacingTowards(quad, Direction.DOWN))
+            {
+                if (dir.getAxis() == Direction.Axis.X)
+                {
+                    boolean cw = (dir.getAxisDirection() == Direction.AxisDirection.NEGATIVE) == type.isTop();
+                    ModelUtils.rotateElement(uv, cw, 1);
+                }
+                else if (dir == Direction.SOUTH)
+                {
+                    ModelUtils.rotateElement(uv, true, 2);
+                }
+
+                int coord2 = dir.getAxis() == Direction.Axis.X ? 0 : 2;
+                if (type == CornerType.HORIZONTAL_TOP_LEFT || type == CornerType.HORIZONTAL_BOTTOM_RIGHT)
+                {
+                    pos[3][1] = pos[2][1];
+                    pos[3][coord2] = pos[2][coord2];
+
+                    uv[3][1] = uv[2][1];
+                    light[3][1] = light[2][1];
+                }
+                else if (type == CornerType.HORIZONTAL_TOP_RIGHT || type == CornerType.HORIZONTAL_BOTTOM_LEFT)
+                {
+                    pos[2][1] = pos[3][1];
+                    pos[2][coord2] = pos[3][coord2];
+
+                    uv[2][1] = uv[3][1];
+                    light[2][1] = light[3][1];
+                }
+            }
+            else
+            {
+                int coord2 = dir.getAxis() == Direction.Axis.X ? 0 : 2;
+                if (type == CornerType.HORIZONTAL_TOP_LEFT || type == CornerType.HORIZONTAL_BOTTOM_RIGHT)
+                {
+                    pos[0][1] = pos[3][1];
+                    pos[0][coord2] = pos[3][coord2];
+
+                    uv[0][1] = uv[3][1];
+                    light[0][1] = light[3][1];
+                }
+                else if (type == CornerType.HORIZONTAL_TOP_RIGHT || type == CornerType.HORIZONTAL_BOTTOM_LEFT)
+                {
+                    pos[1][1] = pos[2][1];
+                    pos[1][coord2] = pos[2][coord2];
+
+                    uv[1][1] = uv[2][1];
+                    light[1][1] = light[2][1];
+                }
+            }
+        }
+    }
+
+    private static void createSideTriangle(BakedQuad quad, float[][] pos, float[][] uv, float[][] light, Direction dir, CornerType type)
+    {
+        if (type.isTop() == (quad.getFace() == dir.rotateY()))
+        {
+            pos[2][1] = pos[1][1];
+            uv[2][1] = uv[1][1];
+            light[2][1] = light[1][1];
+        }
+        else
+        {
+            pos[3][1] = pos[0][1];
+            uv[3][1] = uv[0][1];
+            light[3][1] = light[0][1];
         }
     }
 }
