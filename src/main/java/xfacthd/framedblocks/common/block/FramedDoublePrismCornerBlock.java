cf5b03d2059094cb62545e83bca95b68ab7d8dc3
XFactHD <xfacthd@gmx.de>
2021-07-04 18:59:55 +0200

    Add ability to switch the offset state of double prism corners by hitting them with the hammer

--- src/main/java/xfacthd/framedblocks/common/block/FramedDoublePrismCornerBlock.java
+++ src/main/java/xfacthd/framedblocks/common/block/FramedDoublePrismCornerBlock.java
@@ -2,8 +2,13 @@ package xfacthd.framedblocks.common.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import xfacthd.framedblocks.common.FBContent;
 import xfacthd.framedblocks.common.data.BlockType;
 import xfacthd.framedblocks.common.data.PropertyHolder;
 
@@ -30,5 +35,18 @@ public class FramedDoublePrismCornerBlock extends FramedDoubleThreewayCornerBloc
 
         state = state.with(PropertyHolder.OFFSET, context.getPos().getY() % 2 == 0);
         return state;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void onBlockClicked(BlockState state, World world, BlockPos pos, PlayerEntity player)
+    {
+        if (world.isRemote()) { return; }
+
+        ItemStack stack = player.getHeldItemMainhand();
+        if (stack.getItem() == FBContent.itemFramedHammer.get())
+        {
+            world.setBlockState(pos, state.with(PropertyHolder.OFFSET, !state.get(PropertyHolder.OFFSET)));
+        }
     }
 }
