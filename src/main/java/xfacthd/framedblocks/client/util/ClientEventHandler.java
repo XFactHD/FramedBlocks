7816334b46995614d7ec8eac710882d9d04e2e9b
XFactHD <xfacthd@gmx.de>
2021-07-25 02:20:20 +0200

    Fix custom outlines flickering due to missing normals

--- src/main/java/xfacthd/framedblocks/client/util/ClientEventHandler.java
+++ src/main/java/xfacthd/framedblocks/client/util/ClientEventHandler.java
@@ -2,6 +2,7 @@ package xfacthd.framedblocks.client.util;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.*;
@@ -340,8 +341,16 @@ public class ClientEventHandler
 
     private static void drawLine(VertexConsumer builder, PoseStack mstack, double x1, double y1, double z1, double x2, double y2, double z2)
     {
-        //TODO: calculate proper normal values
-        builder.vertex(mstack.last().pose(), (float)x1, (float)y1, (float)z1).color(0.0F, 0.0F, 0.0F, 0.4F).normal(mstack.last().normal(), 0, 0, 0).endVertex();
-        builder.vertex(mstack.last().pose(), (float)x2, (float)y2, (float)z2).color(0.0F, 0.0F, 0.0F, 0.4F).normal(mstack.last().normal(), 0, 0, 0).endVertex();
+        float nX = (float)(x2 - x1);
+        float nY = (float)(y2 - y1);
+        float nZ = (float)(z2 - z1);
+        float nLen = Mth.sqrt(nX * nX + nY * nY + nZ * nZ);
+
+        nX = nX / nLen;
+        nY = nY / nLen;
+        nZ = nZ / nLen;
+
+        builder.vertex(mstack.last().pose(), (float)x1, (float)y1, (float)z1).color(0.0F, 0.0F, 0.0F, 0.4F).normal(mstack.last().normal(), nX, nY, nZ).endVertex();
+        builder.vertex(mstack.last().pose(), (float)x2, (float)y2, (float)z2).color(0.0F, 0.0F, 0.0F, 0.4F).normal(mstack.last().normal(), nX, nY, nZ).endVertex();
     }
 }
