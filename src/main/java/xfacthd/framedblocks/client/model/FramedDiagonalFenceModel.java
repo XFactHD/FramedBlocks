67b71be157937fa2486b5db810fcb33f122f7451
XFactHD <xfacthd@gmx.de>
2022-04-25 17:17:19 +0200

    Reintroduce support for Diagonal Fences

--- src/main/java/xfacthd/framedblocks/client/model/FramedDiagonalFenceModel.java
+++ src/main/java/xfacthd/framedblocks/client/model/FramedDiagonalFenceModel.java
@@ -1,6 +1,7 @@
 package xfacthd.framedblocks.client.model;
 
 import com.mojang.math.Vector3f;
+import fuzs.diagonalfences.api.IDiagonalBlock;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
@@ -23,12 +24,11 @@ public class FramedDiagonalFenceModel extends FramedFenceModel
     {
         super(state, baseModel);
 
-        //TODO: reactivate when Diagonal Fences is available
-        //boolean hasProps = state.getBlock() instanceof IDiagonalBlock && ((IDiagonalBlock)state.getBlock()).hasProperties();
-        northEast = false; //hasProps && state.getValue(IDiagonalBlock.NORTH_EAST);
-        southEast = false; //hasProps && state.getValue(IDiagonalBlock.SOUTH_EAST);
-        northWest = false; //hasProps && state.getValue(IDiagonalBlock.NORTH_WEST);
-        southWest = false; //hasProps && state.getValue(IDiagonalBlock.SOUTH_WEST);
+        boolean hasProps = state.getBlock() instanceof IDiagonalBlock && ((IDiagonalBlock)state.getBlock()).hasProperties();
+        northEast = hasProps && state.getValue(IDiagonalBlock.NORTH_EAST);
+        southEast = hasProps && state.getValue(IDiagonalBlock.SOUTH_EAST);
+        northWest = hasProps && state.getValue(IDiagonalBlock.NORTH_WEST);
+        southWest = hasProps && state.getValue(IDiagonalBlock.SOUTH_WEST);
     }
 
     @Override
