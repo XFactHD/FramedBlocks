50bca9da6a7181711f1c4d0e5c38175a0f954309
XFactHD <xfacthd@gmx.de>
2022-05-09 00:39:20 +0200

    Add missed import

--- src/main/java/xfacthd/framedblocks/common/block/FramedVerticalStairs.java
+++ src/main/java/xfacthd/framedblocks/common/block/FramedVerticalStairs.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.Half;
 import net.minecraft.world.phys.shapes.*;
+import xfacthd.framedblocks.api.block.FramedBlockEntity;
 import xfacthd.framedblocks.api.util.*;
 import xfacthd.framedblocks.common.data.*;
 
