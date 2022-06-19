e9af8ee3ae981c0b51ebca2cb15c65e3ead440c6
XFactHD <xfacthd@gmx.de>
2022-06-19 19:43:45 +0200

    Fix being unable to climb past trapdoors above Framed Ladders, fixes #123

--- src/main/java/xfacthd/framedblocks/common/block/FramedLadderBlock.java
+++ src/main/java/xfacthd/framedblocks/common/block/FramedLadderBlock.java
@@ -1,26 +1,38 @@
 package xfacthd.framedblocks.common.block;
 
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import xfacthd.framedblocks.api.util.Utils;
 import xfacthd.framedblocks.common.data.BlockType;
 import xfacthd.framedblocks.common.data.PropertyHolder;
 
 public class FramedLadderBlock extends FramedBlock
 {
     private static final VoxelShape SHAPE_NORTH = box( 0, 0,  0, 16, 16,  2);
-    private static final VoxelShape SHAPE_EAST =  box(14, 0,  0, 16, 16, 16);
-    private static final VoxelShape SHAPE_SOUTH = box( 0, 0, 14, 16, 16, 16);
-    private static final VoxelShape SHAPE_WEST =  box( 0, 0,  0,  2, 16, 16);
-    private static final VoxelShape[] SHAPES = new VoxelShape[] { SHAPE_SOUTH, SHAPE_WEST, SHAPE_NORTH, SHAPE_EAST };
+    private static final VoxelShape COLLISION_SHAPE_NORTH = box( 0, 0,  0, 16, 16,  3);
+
+    private static final VoxelShape[] SHAPES = Util.make(new VoxelShape[4], arr ->
+        Direction.Plane.HORIZONTAL.stream().forEach(dir ->
+                arr[dir.get2DDataValue()] = Utils.rotateShape(Direction.NORTH, dir, SHAPE_NORTH)
+        )
+    );
+    private static final VoxelShape[] COLLISION_SHAPES = Util.make(new VoxelShape[4], arr ->
+        Direction.Plane.HORIZONTAL.stream().forEach(dir ->
+                arr[dir.get2DDataValue()] = Utils.rotateShape(Direction.NORTH, dir, COLLISION_SHAPE_NORTH)
+        )
+    );
 
     public FramedLadderBlock() { super(BlockType.FRAMED_LADDER); }
 
@@ -44,5 +56,18 @@ public class FramedLadderBlock extends FramedBlock
     }
 
     @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+    {
+        return COLLISION_SHAPES[state.getValue(PropertyHolder.FACING_HOR).get2DDataValue()];
+    }
+
+    @Override
     public boolean isLadder(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) { return true; }
+
+    @Override
+    public boolean makesOpenTrapdoorAboveClimbable(BlockState state, LevelReader level, BlockPos pos, BlockState trapdoorState)
+    {
+        return state.getValue(PropertyHolder.FACING_HOR) == trapdoorState.getValue(TrapDoorBlock.FACING).getOpposite();
+    }
 }
