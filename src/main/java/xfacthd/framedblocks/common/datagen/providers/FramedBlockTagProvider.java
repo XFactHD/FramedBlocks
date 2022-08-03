75f3f49b380a5a1f618bcde246f77eadf1aa1683
XFactHD <xfacthd@gmx.de>
2022-08-04 00:55:30 +0200

    Add missing tags to signs

--- src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedBlockTagProvider.java
+++ src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedBlockTagProvider.java
@@ -32,6 +32,9 @@ public class FramedBlockTagProvider extends BlockTagsProvider
         tag(BlockTags.DOORS).add(FBContent.blockFramedDoor.get(), FBContent.blockFramedIronDoor.get());
         tag(BlockTags.TRAPDOORS).add(FBContent.blockFramedTrapDoor.get(), FBContent.blockFramedIronTrapDoor.get());
         tag(BlockTags.CLIMBABLE).add(FBContent.blockFramedLadder.get());
+        tag(BlockTags.SIGNS).add(FBContent.blockFramedSign.get(), FBContent.blockFramedWallSign.get());
+        tag(BlockTags.STANDING_SIGNS).add(FBContent.blockFramedSign.get());
+        tag(BlockTags.WALL_SIGNS).add(FBContent.blockFramedWallSign.get());
         tag(Tags.Blocks.CHESTS).add(FBContent.blockFramedChest.get());
         tag(BlockTags.RAILS).add(FBContent.blockFramedRailSlope.get());
 
