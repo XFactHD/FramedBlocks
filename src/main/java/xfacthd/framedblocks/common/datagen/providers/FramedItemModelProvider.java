cae1a1ea0b4b799a8e0fd8e5710fb4951a974164
XFactHD <xfacthd@gmx.de>
2022-10-26 23:45:49 +0200

    Use item/handheld for tool items to avoid weird mirroring in the off-hand

--- src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedItemModelProvider.java
+++ src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedItemModelProvider.java
@@ -1,11 +1,15 @@
 package xfacthd.framedblocks.common.datagen.providers;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.*;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 import xfacthd.framedblocks.api.util.FramedConstants;
 import xfacthd.framedblocks.client.util.BlueprintPropertyOverride;
+import xfacthd.framedblocks.common.FBContent;
 
+@SuppressWarnings({ "SameParameterValue", "UnusedReturnValue" })
 public class FramedItemModelProvider extends ItemModelProvider
 {
     public FramedItemModelProvider(DataGenerator gen, ExistingFileHelper fileHelper)
@@ -16,13 +20,13 @@ public class FramedItemModelProvider extends ItemModelProvider
     @Override
     protected void registerModels()
     {
-        singleTexture("framed_hammer", mcLoc("item/generated"), "layer0", modLoc("item/framed_hammer"));
-        singleTexture("framed_wrench", mcLoc("item/generated"), "layer0", modLoc("item/framed_wrench"));
-        singleTexture("framed_key", mcLoc("item/generated"), "layer0", modLoc("item/framed_key"));
-        singleTexture("framed_screwdriver", mcLoc("item/generated"), "layer0", modLoc("item/framed_screwdriver"));
+        handheldItem(FBContent.itemFramedHammer);
+        handheldItem(FBContent.itemFramedWrench);
+        handheldItem(FBContent.itemFramedKey);
+        handheldItem(FBContent.itemFramedScrewdriver);
 
-        ItemModelBuilder modelNormal = singleTexture("framed_blueprint", mcLoc("item/generated"), "layer0", modLoc("item/framed_blueprint"));
-        ModelFile modelWritten = singleTexture("framed_blueprint_written", mcLoc("item/generated"), "layer0", modLoc("item/framed_blueprint_written"));
+        ItemModelBuilder modelNormal = simpleItem(FBContent.itemFramedBlueprint);
+        ModelFile modelWritten = simpleItem("framed_blueprint_written");
 
         modelNormal.override()
                     .predicate(BlueprintPropertyOverride.HAS_DATA, 0)
@@ -32,5 +36,21 @@ public class FramedItemModelProvider extends ItemModelProvider
                     .predicate(BlueprintPropertyOverride.HAS_DATA, 1)
                     .model(modelWritten)
                     .end();
+    }
+
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item)
+    {
+        String name = item.getId().getPath();
+        return singleTexture(name, mcLoc("item/handheld"), "layer0", modLoc("item/" + name));
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item)
+    {
+        return simpleItem(item.getId().getPath());
+    }
+
+    private ItemModelBuilder simpleItem(String name)
+    {
+        return singleTexture(name, mcLoc("item/generated"), "layer0", modLoc("item/" + name));
     }
 }
