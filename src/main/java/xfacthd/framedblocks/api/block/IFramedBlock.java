dfa22a6228d1d788fe1fde9f4527d651e92a3111
XFactHD <xfacthd@gmx.de>
2022-11-28 17:31:25 +0100

    Only get light level from BE if block can't occlude or has the glowing state set

--- src/main/java/xfacthd/framedblocks/api/block/IFramedBlock.java
+++ src/main/java/xfacthd/framedblocks/api/block/IFramedBlock.java
@@ -147,6 +147,20 @@ public interface IFramedBlock extends EntityBlock//, IFacade
         return InteractionResult.FAIL;
     }
 
+    default int getLight(BlockState state, BlockGetter level, BlockPos pos)
+    {
+        //TODO: 1.20: require glowing state on all blocks
+        if (getBlockType().canOccludeWithSolidCamo() && !state.getValue(FramedProperties.GLOWING))
+        {
+            return 0;
+        }
+        if (level.getBlockEntity(pos) instanceof FramedBlockEntity be)
+        {
+            return be.getLightValue();
+        }
+        return 0;
+    }
+
     default int getLight(BlockGetter level, BlockPos pos)
     {
         if (level.getBlockEntity(pos) instanceof FramedBlockEntity be)
