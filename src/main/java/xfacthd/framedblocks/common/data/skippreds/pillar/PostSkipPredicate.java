f47c9cde2f5cbd2d4b3c6f3a518943ccb27fe461
XFactHD <xfacthd@gmx.de>
2023-01-07 03:20:03 +0100

    Fix fence skip predicate and add interaction with Framed Posts

--- src/main/java/xfacthd/framedblocks/common/data/skippreds/pillar/PostSkipPredicate.java
+++ src/main/java/xfacthd/framedblocks/common/data/skippreds/pillar/PostSkipPredicate.java
@@ -6,6 +6,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import xfacthd.framedblocks.api.util.SideSkipPredicate;
+import xfacthd.framedblocks.common.FBContent;
 
 public final class PostSkipPredicate implements SideSkipPredicate
 {
@@ -13,12 +14,20 @@ public final class PostSkipPredicate implements SideSkipPredicate
     public boolean test(BlockGetter level, BlockPos pos, BlockState state, BlockState adjState, Direction side)
     {
         Direction.Axis axis = state.getValue(BlockStateProperties.AXIS);
-        if (side == null || side.getAxis() != axis || adjState.getBlock() != state.getBlock())
+        if (side == null || side.getAxis() != axis)
         {
             return false;
         }
 
-        Direction.Axis adjAxis = adjState.getValue(BlockStateProperties.AXIS);
-        return axis == adjAxis && SideSkipPredicate.compareState(level, pos, side);
+        if (adjState.getBlock() == state.getBlock())
+        {
+            Direction.Axis adjAxis = adjState.getValue(BlockStateProperties.AXIS);
+            return axis == adjAxis && SideSkipPredicate.compareState(level, pos, side);
+        }
+        else if (adjState.getBlock() == FBContent.blockFramedFence.get())
+        {
+            return axis == Direction.Axis.Y && SideSkipPredicate.compareState(level, pos, side);
+        }
+        return false;
     }
 }
