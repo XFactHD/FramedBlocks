f7e0b60415c4b81c2613229ab10e48cb8ab62f65
XFactHD <xfacthd@gmx.de>
2023-03-11 01:55:31 +0100

    Add Framed Key to items that suppress intangibility

--- src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedItemTagProvider.java
+++ src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedItemTagProvider.java
@@ -38,7 +38,8 @@ public final class FramedItemTagProvider extends ItemTagsProvider
         tag(Utils.DISABLE_INTANGIBLE).addTag(Utils.WRENCH).add(
                 FBContent.itemFramedHammer.get(),
                 FBContent.itemFramedBlueprint.get(),
-                FBContent.itemFramedScrewdriver.get()
+                FBContent.itemFramedScrewdriver.get(),
+                FBContent.itemFramedKey.get()
         );
     }
 }
