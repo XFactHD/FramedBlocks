368ef71411c2234877312f46bb9755856f7e1691
XFactHD <xfacthd@gmx.de>
2023-04-08 20:40:31 +0200

    Prevent potential recursive render type cache update

--- src/main/java/xfacthd/framedblocks/client/model/cube/FramedCubeModel.java
+++ src/main/java/xfacthd/framedblocks/client/model/cube/FramedCubeModel.java
@@ -1,12 +1,27 @@
 package xfacthd.framedblocks.client.model.cube;
 
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.ChunkRenderTypeSet;
+import net.minecraftforge.client.model.data.ModelData;
+import org.jetbrains.annotations.NotNull;
 import xfacthd.framedblocks.api.util.FramedBlockData;
 
 public class FramedCubeModel extends FramedCubeBaseModel
 {
     public FramedCubeModel(BlockState state, BakedModel baseModel) { super(state, baseModel); }
+
+    @Override
+    public ChunkRenderTypeSet getRenderTypes(@NotNull BlockState state, @NotNull RandomSource rand, @NotNull ModelData data)
+    {
+        FramedBlockData fbData = data.get(FramedBlockData.PROPERTY);
+        if (fbData == null || fbData.getCamoState().isAir())
+        {
+            return baseModel.getRenderTypes(state, rand, data);
+        }
+        return super.getRenderTypes(state, rand, data);
+    }
 
     @Override
     protected BlockState getNoCamoModelState(BlockState camoState, FramedBlockData fbData)
