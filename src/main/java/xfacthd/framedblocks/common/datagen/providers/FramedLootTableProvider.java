dcb2afe582e96a6ac14595f0fdb6914b4af73610
XFactHD <xfacthd@gmx.de>
2023-06-19 16:27:45 +0200

    Simplify block loot table generation

--- src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedLootTableProvider.java
+++ src/main/java/xfacthd/framedblocks/common/datagen/providers/FramedLootTableProvider.java
@@ -5,8 +5,6 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.storage.loot.*;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
@@ -14,8 +12,6 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.registries.RegistryObject;
 import xfacthd.framedblocks.common.FBContent;
-import xfacthd.framedblocks.common.block.interactive.FramedWaterloggablePressurePlateBlock;
-import xfacthd.framedblocks.common.block.interactive.FramedWaterloggableWeightedPressurePlateBlock;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -48,17 +44,6 @@ public final class FramedLootTableProvider extends LootTableProvider
         @Override
         protected void generate()
         {
-            FBContent.getRegisteredBlocks()
-                    .stream()
-                    .map(RegistryObject::get)
-                    .filter(block -> block != FBContent.BLOCK_FRAMED_DOOR.get() &&
-                            block != FBContent.BLOCK_FRAMED_DOUBLE_SLAB.get() &&
-                            block != FBContent.BLOCK_FRAMED_DOUBLE_PANEL.get() &&
-                            block != FBContent.BLOCK_FRAMED_IRON_DOOR.get() &&
-                            !(block instanceof FramedWaterloggablePressurePlateBlock) &&
-                            !(block instanceof FramedWaterloggableWeightedPressurePlateBlock)
-                    )
-                    .forEach(this::dropSelf);
 
             dropOther(FBContent.BLOCK_FRAMED_WATERLOGGABLE_PRESSURE_PLATE.get(), FBContent.BLOCK_FRAMED_PRESSURE_PLATE.get());
             dropOther(FBContent.BLOCK_FRAMED_WATERLOGGABLE_STONE_PRESSURE_PLATE.get(), FBContent.BLOCK_FRAMED_STONE_PRESSURE_PLATE.get());
@@ -66,24 +51,36 @@ public final class FramedLootTableProvider extends LootTableProvider
             dropOther(FBContent.BLOCK_FRAMED_WATERLOGGABLE_GOLD_PRESSURE_PLATE.get(), FBContent.BLOCK_FRAMED_GOLD_PRESSURE_PLATE.get());
             dropOther(FBContent.BLOCK_FRAMED_WATERLOGGABLE_IRON_PRESSURE_PLATE.get(), FBContent.BLOCK_FRAMED_IRON_PRESSURE_PLATE.get());
 
-            add(FBContent.BLOCK_FRAMED_DOOR.get(), block -> createSinglePropConditionTable(block, DoorBlock.HALF, DoubleBlockHalf.LOWER));
-            add(FBContent.BLOCK_FRAMED_DOUBLE_SLAB.get(), block -> droppingTwo(block, FBContent.BLOCK_FRAMED_SLAB.get()));
-            add(FBContent.BLOCK_FRAMED_DOUBLE_PANEL.get(), block -> droppingTwo(block, FBContent.BLOCK_FRAMED_PANEL.get()));
-            add(FBContent.BLOCK_FRAMED_IRON_DOOR.get(), block -> createSinglePropConditionTable(block, DoorBlock.HALF, DoubleBlockHalf.LOWER));
+            dropDoor(FBContent.BLOCK_FRAMED_DOOR.get());
+            dropDoor(FBContent.BLOCK_FRAMED_IRON_DOOR.get());
+            dropTwoOf(FBContent.BLOCK_FRAMED_DOUBLE_SLAB.get(), FBContent.BLOCK_FRAMED_SLAB.get());
+            dropTwoOf(FBContent.BLOCK_FRAMED_DOUBLE_PANEL.get(), FBContent.BLOCK_FRAMED_PANEL.get());
 
             dropOther(FBContent.BLOCK_FRAMED_VERTICAL_HALF_SLOPE.get(), FBContent.BLOCK_FRAMED_HALF_SLOPE.get());
             dropOther(FBContent.BLOCK_FRAMED_VERTICAL_DOUBLE_HALF_SLOPE.get(), FBContent.BLOCK_FRAMED_DOUBLE_HALF_SLOPE.get());
+
+            FBContent.getRegisteredBlocks()
+                    .stream()
+                    .map(RegistryObject::get)
+                    .filter(block -> !map.containsKey(block.getLootTable()))
+                    .forEach(this::dropSelf);
         }
 
-        protected LootTable.Builder droppingTwo(Block block, Block drop) {
-            return LootTable.lootTable().withPool(
+        protected void dropTwoOf(Block block, Block drop)
+        {
+            add(block, b -> LootTable.lootTable().withPool(
                     LootPool.lootPool().setRolls(ConstantValue.exactly(1)).add(
                             applyExplosionDecay(block, LootItem.lootTableItem(drop).apply(
-                                    SetItemCountFunction.setCount(ConstantValue.exactly(2))
+                                            SetItemCountFunction.setCount(ConstantValue.exactly(2))
                                     )
                             )
                     )
-            );
+            ));
+        }
+
+        protected void dropDoor(Block block)
+        {
+            add(block, this::createDoorTable);
         }
     }
 }
