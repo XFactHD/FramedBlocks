43251375245e117a998b7e0622737eb906ad9a66
XFactHD <xfacthd@gmx.de>
2023-12-17 03:29:29 +0100

    Fix ModernFix integration crashing without ModernFix

--- src/main/java/xfacthd/framedblocks/common/compat/modernfix/ModernFixCompat.java
+++ src/main/java/xfacthd/framedblocks/common/compat/modernfix/ModernFixCompat.java
@@ -1,5 +1,6 @@
 package xfacthd.framedblocks.common.compat.modernfix;
 
+import net.neoforged.fml.ModList;
 import org.embeddedt.modernfix.core.ModernFixMixinPlugin;
 
 public final class ModernFixCompat
@@ -8,12 +9,25 @@ public final class ModernFixCompat
 
     public static void init()
     {
-        dynamicResources = ModernFixMixinPlugin.instance.isOptionEnabled("perf.dynamic_resources.FramedBlocksEnabledCheck");
+        if (ModList.get().isLoaded("modernfix"))
+        {
+            Guarded.init();
+        }
     }
 
     public static boolean dynamicResourcesEnabled()
     {
         return dynamicResources;
+    }
+
+
+
+    private static final class Guarded
+    {
+        public static void init()
+        {
+            dynamicResources = ModernFixMixinPlugin.instance.isOptionEnabled("perf.dynamic_resources.FramedBlocksEnabledCheck");
+        }
     }
 
 
