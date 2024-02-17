f277cce1f5bd32ad8eac046468d48fbfa11f0ec7
XFactHD <xfacthd@gmx.de>
2024-02-17 01:27:07 +0100

    Take over wall shapes entirely, replaces hacky shape map fixing

--- src/main/java/xfacthd/framedblocks/common/block/pillar/FramedWallBlock.java
+++ src/main/java/xfacthd/framedblocks/common/block/pillar/FramedWallBlock.java
@@ -1,6 +1,6 @@
 package xfacthd.framedblocks.common.block.pillar;
 
-import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -14,14 +14,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.WallSide;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.*;
 import net.minecraftforge.client.extensions.common.IClientBlockExtensions;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import xfacthd.framedblocks.api.block.FramedProperties;
 import xfacthd.framedblocks.api.block.IFramedBlock;
 import xfacthd.framedblocks.api.block.render.FramedBlockRenderProperties;
+import xfacthd.framedblocks.api.shapes.*;
 import xfacthd.framedblocks.common.data.BlockType;
 
 import javax.annotation.Nullable;
@@ -32,6 +34,9 @@ import java.util.function.Consumer;
 @SuppressWarnings("deprecation")
 public class FramedWallBlock extends WallBlock implements IFramedBlock
 {
+    private final ShapeProvider shapes = makeShapeProvider(states -> generateShapes(states, 14F, 16F));
+    private final ShapeProvider collisionShapes = makeShapeProvider(states -> generateShapes(states, 24F, 24F));
+
     public FramedWallBlock()
     {
         super(IFramedBlock.createProperties(BlockType.FRAMED_WALL));
@@ -40,7 +45,6 @@ public class FramedWallBlock extends WallBlock implements IFramedBlock
                 .setValue(FramedProperties.GLOWING, false)
                 .setValue(FramedProperties.PROPAGATES_SKYLIGHT, false)
         );
-        fixShapeMaps();
     }
 
     @Override
@@ -95,6 +99,18 @@ public class FramedWallBlock extends WallBlock implements IFramedBlock
     }
 
     @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx)
+    {
+        return shapes.get(state);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx)
+    {
+        return collisionShapes.get(state);
+    }
+
+    @Override
     public float getShadeBrightness(BlockState state, BlockGetter level, BlockPos pos)
     {
         return getCamoShadeBrightness(state, level, pos, super.getShadeBrightness(state, level, pos));
@@ -130,38 +146,103 @@ public class FramedWallBlock extends WallBlock implements IFramedBlock
         consumer.accept(FramedBlockRenderProperties.INSTANCE);
     }
 
-
-
-    private void fixShapeMaps()
+    @Override
+    protected Map<BlockState, VoxelShape> makeShapes(float pWidth, float pDepth, float pWallPostHeight, float pWallMinY, float pWallLowHeight, float pWallTallHeight)
     {
-        Map<BlockState, VoxelShape> shapeByIndex = ObfuscationReflectionHelper.getPrivateValue(WallBlock.class, this, "f_57955_");
-        shapeByIndex = fixShapeMap(shapeByIndex);
-        ObfuscationReflectionHelper.setPrivateValue(WallBlock.class, this, shapeByIndex, "f_57955_");
-
-        Map<BlockState, VoxelShape> collisionShapeByIndex = ObfuscationReflectionHelper.getPrivateValue(WallBlock.class, this, "f_57956_");
-        collisionShapeByIndex = fixShapeMap(collisionShapeByIndex);
-        ObfuscationReflectionHelper.setPrivateValue(WallBlock.class, this, collisionShapeByIndex, "f_57956_");
+        // Effectively NO-OP to conserve memory, the shape building is taken over below
+        return Map.of();
     }
 
-    private static Map<BlockState, VoxelShape> fixShapeMap(Map<BlockState, VoxelShape> map)
+    private ShapeProvider makeShapeProvider(ShapeGenerator generator)
     {
-        Preconditions.checkNotNull(map, "Got a null map?!");
+        ImmutableList<BlockState> states = stateDefinition.getPossibleStates();
+        if (!FMLEnvironment.production)
+        {
+            return new ReloadableShapeProvider(generator, states);
+        }
+        return generator.generate(states);
+    }
+
+
+
+    private static ShapeProvider generateShapes(
+            ImmutableList<BlockState> states,
+            float lowHeight,
+            float tallHeight
+    )
+    {
+        boolean sameHeight = lowHeight == tallHeight;
+
+        VoxelShape centerLowShape = box(5F, 0F, 5F, 11F, lowHeight, 11F);
+        VoxelShape centerTallShape = sameHeight ? centerLowShape : box(5F, 0F, 5F, 11F, tallHeight, 11F);
+        VoxelShape postShape = box(4F, 0F, 4F, 12F, tallHeight, 12F);
+        VoxelShape wallLowShape = box(5F, 0F, 0F, 11F, lowHeight, 5F);
+        VoxelShape wallTallShape = sameHeight ? wallLowShape : box(5F, 0F, 0F, 11F, tallHeight, 5F);
+
+        VoxelShape[] wallLowShapes = ShapeUtils.makeHorizontalRotations(wallLowShape, Direction.NORTH);
+        VoxelShape[] wallTallShapes = sameHeight ? wallLowShapes : ShapeUtils.makeHorizontalRotations(wallTallShape, Direction.NORTH);
+
+        VoxelShape[] shapes = new VoxelShape[512];
+        for (WallSide north : NORTH_WALL.getPossibleValues())
+        {
+            for (WallSide east : EAST_WALL.getPossibleValues())
+            {
+                for (WallSide south : SOUTH_WALL.getPossibleValues())
+                {
+                    for (WallSide west : WEST_WALL.getPossibleValues())
+                    {
+                        int noUpKey = makeShapeKey(false, north, east, south, west);
+                        int upKey = makeShapeKey(true, north, east, south, west);
+
+                        boolean anyTall = north == WallSide.TALL || east == WallSide.TALL || south == WallSide.TALL || west == WallSide.TALL;
+                        shapes[noUpKey] = ShapeUtils.or(
+                                anyTall ? centerTallShape : centerLowShape,
+                                getWallShape(north, Direction.NORTH, wallLowShapes, wallTallShapes),
+                                getWallShape(east, Direction.EAST, wallLowShapes, wallTallShapes),
+                                getWallShape(south, Direction.SOUTH, wallLowShapes, wallTallShapes),
+                                getWallShape(west, Direction.WEST, wallLowShapes, wallTallShapes)
+                        );
+
+                        shapes[upKey] = ShapeUtils.or(
+                                postShape,
+                                getWallShape(north, Direction.NORTH, wallLowShapes, wallTallShapes),
+                                getWallShape(east, Direction.EAST, wallLowShapes, wallTallShapes),
+                                getWallShape(south, Direction.SOUTH, wallLowShapes, wallTallShapes),
+                                getWallShape(west, Direction.WEST, wallLowShapes, wallTallShapes)
+                        );
+                    }
+                }
+            }
+        }
+        shapes[0] = Shapes.block();
 
         ImmutableMap.Builder<BlockState, VoxelShape> builder = ImmutableMap.builder();
-        builder.putAll(map);
-
-        for (BlockState state : map.keySet())
+        for (BlockState state : states)
         {
-            VoxelShape shape = map.get(state);
-            builder.put(state.cycle(FramedProperties.STATE_LOCKED), shape);
-            builder.put(state.cycle(FramedProperties.GLOWING), shape);
-            builder.put(state.cycle(FramedProperties.PROPAGATES_SKYLIGHT), shape);
-            builder.put(state.cycle(FramedProperties.GLOWING).cycle(FramedProperties.STATE_LOCKED), shape);
-            builder.put(state.cycle(FramedProperties.PROPAGATES_SKYLIGHT).cycle(FramedProperties.STATE_LOCKED), shape);
-            builder.put(state.cycle(FramedProperties.PROPAGATES_SKYLIGHT).cycle(FramedProperties.GLOWING), shape);
-            builder.put(state.cycle(FramedProperties.PROPAGATES_SKYLIGHT).cycle(FramedProperties.GLOWING).cycle(FramedProperties.STATE_LOCKED), shape);
+            int key = makeShapeKey(
+                    state.getValue(UP),
+                    state.getValue(NORTH_WALL),
+                    state.getValue(EAST_WALL),
+                    state.getValue(SOUTH_WALL),
+                    state.getValue(WEST_WALL)
+            );
+            builder.put(state, shapes[key]);
         }
+        return ShapeProvider.of(builder.build());
+    }
 
-        return builder.build();
+    private static VoxelShape getWallShape(WallSide side, Direction dir, VoxelShape[] lowShapes, VoxelShape[] tallShapes)
+    {
+        return switch (side)
+        {
+            case NONE -> Shapes.empty();
+            case LOW -> lowShapes[dir.get2DDataValue()];
+            case TALL -> tallShapes[dir.get2DDataValue()];
+        };
+    }
+
+    private static int makeShapeKey(boolean up, WallSide north, WallSide east, WallSide south, WallSide west)
+    {
+        return ((up ? 1 : 0) << 8) | (north.ordinal() << 6) | (east.ordinal() << 4) | (south.ordinal() << 2) | west.ordinal();
     }
 }
