1b5e11fef1ece0065196c3a9df0eb32eb7715690
XFactHD <xfacthd@gmx.de>
2024-02-19 04:24:49 +0100

    Slightly speed up StateCache generation

--- src/main/java/xfacthd/framedblocks/common/data/StateCacheBuilder.java
+++ src/main/java/xfacthd/framedblocks/common/data/StateCacheBuilder.java
@@ -4,6 +4,7 @@ import com.google.common.base.Stopwatch;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraftforge.registries.ForgeRegistries;
 import xfacthd.framedblocks.FramedBlocks;
@@ -36,34 +37,34 @@ public final class StateCacheBuilder
     {
         FramedBlocks.LOGGER.debug("Initializing custom state metadata caches");
         Stopwatch watch = Stopwatch.createStarted();
-        Map<Block, List<StateCache>> dedupMap = new HashMap<>();
-        long count = ForgeRegistries.BLOCKS.getValues()
+        long[] counts = new long[] { 0, 1 }; // +1 for the empty instance
+        ForgeRegistries.BLOCKS.getValues()
                 .stream()
                 .filter(block -> block instanceof IFramedBlock)
                 .map(Block::getStateDefinition)
                 .map(StateDefinition::getPossibleStates)
-                .flatMap(List::stream)
-                .peek(state ->
+                .forEach(states ->
                 {
-                    StateCache cache = ((IFramedBlock) state.getBlock()).initCache(state);
-                    if (cache.equals(StateCache.EMPTY))
+                    Map<StateCache, StateCache> cacheDedup = new HashMap<>();
+                    for (BlockState state : states)
                     {
-                        ((IStateCacheAccessor) state).framedblocks$initCache(StateCache.EMPTY);
-                        return;
+                        StateCache cache = ((IFramedBlock) state.getBlock()).initCache(state);
+                        if (cache.equals(StateCache.EMPTY))
+                        {
+                            ((IStateCacheAccessor) state).framedblocks$initCache(StateCache.EMPTY);
+                        }
+                        else
+                        {
+                            ((IStateCacheAccessor) state).framedblocks$initCache(
+                                    Objects.requireNonNullElse(cacheDedup.putIfAbsent(cache, cache), cache)
+                            );
+                        }
                     }
-
-                    List<StateCache> caches = dedupMap.computeIfAbsent(state.getBlock(), $ -> new ArrayList<>());
-                    StateCache deduped = caches.stream().filter(cache::equals).findFirst().orElseGet(() ->
-                    {
-                        caches.add(cache);
-                        return cache;
-                    });
-                    ((IStateCacheAccessor) state).framedblocks$initCache(deduped);
-                })
-                .count();
+                    counts[0] += states.size();
+                    counts[1] += cacheDedup.size();
+                });
         watch.stop();
-        long unique = dedupMap.values().stream().mapToLong(List::size).sum() + 1; // +1 for the empty instance
-        FramedBlocks.LOGGER.debug("Initialized {} unique caches for {} states in {}", unique, count, watch);
+        FramedBlocks.LOGGER.debug("Initialized {} unique caches for {} states in {}", counts[1], counts[0], watch);
     }
 
 
