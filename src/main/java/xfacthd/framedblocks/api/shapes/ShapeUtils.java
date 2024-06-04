77a360b19b6b85a437988fa2ea42236292bfec0f
XFactHD <xfacthd@gmx.de>
2024-06-04 23:39:46 +0200

    Add VoxelShape rotation helpers around X and Z axis

--- src/main/java/xfacthd/framedblocks/api/shapes/ShapeUtils.java
+++ src/main/java/xfacthd/framedblocks/api/shapes/ShapeUtils.java
@@ -1,5 +1,6 @@
 package xfacthd.framedblocks.api.shapes;
 
+import net.minecraft.Util;
 import net.minecraft.core.Direction;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.shapes.*;
@@ -11,6 +12,24 @@ import java.util.Map;
 public final class ShapeUtils
 {
     private static final Direction[] HORIZONTAL_DIRECTIONS = Direction.Plane.HORIZONTAL.stream().toArray(Direction[]::new);
+    private static final int[] DIR_ROT_X_2D_DATA = Util.make(new int[6], arr ->
+    {
+        arr[Direction.DOWN.ordinal()] = 2;
+        arr[Direction.UP.ordinal()] = 0;
+        arr[Direction.NORTH.ordinal()] = 3;
+        arr[Direction.SOUTH.ordinal()] = 1;
+        arr[Direction.WEST.ordinal()] = -1;
+        arr[Direction.EAST.ordinal()] = -1;
+    });
+    private static final int[] DIR_ROT_Z_2D_DATA = Util.make(new int[6], arr ->
+    {
+        arr[Direction.DOWN.ordinal()] = 2;
+        arr[Direction.UP.ordinal()] = 0;
+        arr[Direction.NORTH.ordinal()] = -1;
+        arr[Direction.SOUTH.ordinal()] = -1;
+        arr[Direction.WEST.ordinal()] = 3;
+        arr[Direction.EAST.ordinal()] = 1;
+    });
 
     public static VoxelShape orUnoptimized(VoxelShape first, VoxelShape second)
     {
@@ -84,6 +103,69 @@ public final class ShapeUtils
             for (int i = 0; i < times; i++)
             {
                 box = new AABB(1 - box.maxZ, box.minY, box.minX, 1 - box.minZ, box.maxY, box.maxX);
+            }
+            rotatedShape = orUnoptimized(rotatedShape, Shapes.create(box));
+        }
+
+        return rotatedShape;
+    }
+
+    public static VoxelShape rotateShapeAroundX(Direction from, Direction to, VoxelShape shape)
+    {
+        return rotateShapeUnoptimizedAroundX(from, to, shape).optimize();
+    }
+
+    public static VoxelShape rotateShapeUnoptimizedAroundX(Direction from, Direction to, VoxelShape shape)
+    {
+        if (Utils.isX(from) || Utils.isX(to))
+        {
+            throw new IllegalArgumentException("Invalid Direction!");
+        }
+        if (from == to)
+        {
+            return shape;
+        }
+
+        List<AABB> sourceBoxes = shape.toAabbs();
+        VoxelShape rotatedShape = Shapes.empty();
+        int times = (DIR_ROT_X_2D_DATA[to.ordinal()] - DIR_ROT_X_2D_DATA[from.ordinal()] + 4) % 4;
+        for (AABB box : sourceBoxes)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                box = new AABB(box.minX, 1 - box.maxZ, box.minY, box.maxX, 1 - box.minZ, box.maxY);
+            }
+            rotatedShape = orUnoptimized(rotatedShape, Shapes.create(box));
+        }
+
+        return rotatedShape;
+    }
+
+    public static VoxelShape rotateShapeAroundZ(Direction from, Direction to, VoxelShape shape)
+    {
+        return rotateShapeUnoptimizedAroundZ(from, to, shape).optimize();
+    }
+
+    public static VoxelShape rotateShapeUnoptimizedAroundZ(Direction from, Direction to, VoxelShape shape)
+    {
+        if (Utils.isZ(from) || Utils.isZ(to))
+        {
+            throw new IllegalArgumentException("Invalid Direction!");
+        }
+        if (from == to)
+        {
+            return shape;
+        }
+
+        List<AABB> sourceBoxes = shape.toAabbs();
+        VoxelShape rotatedShape = Shapes.empty();
+        int times = (DIR_ROT_Z_2D_DATA[to.ordinal()] - DIR_ROT_Z_2D_DATA[from.ordinal()] + 4) % 4;
+        for (AABB box : sourceBoxes)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                //noinspection SuspiciousNameCombination
+                box = new AABB(box.minY, 1 - box.maxX, box.minZ, box.maxY, 1 - box.minX, box.maxZ);
             }
             rotatedShape = orUnoptimized(rotatedShape, Shapes.create(box));
         }
