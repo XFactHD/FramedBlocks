d7ea9f3d8c461dfaf6cc646d641377a16776876b
XFactHD <xfacthd@gmx.de>
2024-06-08 00:57:09 +0200

    Document WrapHelper

--- src/main/java/xfacthd/framedblocks/api/model/wrapping/WrapHelper.java
+++ src/main/java/xfacthd/framedblocks/api/model/wrapping/WrapHelper.java
@@ -6,11 +6,13 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.Property;
 import xfacthd.framedblocks.api.block.FramedProperties;
 import xfacthd.framedblocks.api.internal.InternalClientAPI;
+import xfacthd.framedblocks.api.model.geometry.Geometry;
 import xfacthd.framedblocks.api.model.wrapping.statemerger.StateMerger;
 import xfacthd.framedblocks.api.util.Utils;
 
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public final class WrapHelper
 {
     /** List of properties which are always present and always need to be ignored */
@@ -28,31 +30,97 @@ public final class WrapHelper
     /** {@link WrapHelper#IGNORE_ALWAYS} + solid + waterlogged + state-lock */
     public static final Set<Property<?>> IGNORE_DEFAULT_LOCK = Utils.concat(Set.of(FramedProperties.STATE_LOCKED), IGNORE_DEFAULT);
 
+    /**
+     * Wrap the models of all states of the given block with models generated from {@link Geometry}s created by
+     * the given {@link GeometryFactory}.
+     * <p>
+     * States which match an already wrapped state after resetting the given ignored properties to default values
+     * will re-use the existing wrapped model
+     *
+     * @param block The block whose models to wrap
+     * @param blockGeometryFactory The {@link GeometryFactory} to generate the wrapping models with
+     * @param ignoredProps The state properties to ignore during wrapping
+     */
     public static void wrap(Holder<Block> block, GeometryFactory blockGeometryFactory, Set<Property<?>> ignoredProps)
     {
         wrap(block, blockGeometryFactory, StateMerger.ignoring(ignoredProps));
     }
 
+    /**
+     * Wrap the models of all states of the given block with models generated from {@link Geometry}s created by
+     * the given {@link GeometryFactory}.
+     * <p>
+     * States which match an already wrapped state after applying the given {@link StateMerger} will re-use the
+     * existing wrapped model
+     *
+     * @param block The block whose models to wrap
+     * @param blockGeometryFactory The {@link GeometryFactory} to generate the wrapping models with
+     * @param stateMerger The {@link StateMerger} to use for merging visually redundant states during wrapping
+     */
     public static void wrap(Holder<Block> block, GeometryFactory blockGeometryFactory, StateMerger stateMerger)
     {
         InternalClientAPI.INSTANCE.registerModelWrapper(block, blockGeometryFactory, stateMerger);
     }
 
+    /**
+     * Wrap the models of all states of the given block with models generated from {@link Geometry}s created by
+     * the given {@link GeometryFactory}.
+     * <p>
+     * States which match an already wrapped state after resetting the given ignored properties to default values
+     * will re-use the existing wrapped model
+     *
+     * @param block The block whose models to wrap
+     * @param modelFactory The {@link ModelFactory} to generate the wrapping models with
+     * @param ignoredProps The state properties to ignore during wrapping
+     */
     public static void wrapSpecial(Holder<Block> block, ModelFactory modelFactory, Set<Property<?>> ignoredProps)
     {
         wrapSpecial(block, modelFactory, StateMerger.ignoring(ignoredProps));
     }
 
+    /**
+     * Wrap the models of all states of the given block with models generated from {@link Geometry}s created by
+     * the given {@link GeometryFactory}.
+     * <p>
+     * States which match an already wrapped state after applying the given {@link StateMerger} will re-use the
+     * existing wrapped model
+     *
+     * @param block The block whose models to wrap
+     * @param modelFactory The {@link ModelFactory} to generate the wrapping models with
+     * @param stateMerger The {@link StateMerger} to use for merging visually redundant states during wrapping
+     */
     public static void wrapSpecial(Holder<Block> block, ModelFactory modelFactory, StateMerger stateMerger)
     {
         InternalClientAPI.INSTANCE.registerSpecialModelWrapper(block, modelFactory, stateMerger);
     }
 
+    /**
+     * Re-use the wrapped models from the given source block for the given block.
+     * <p>
+     * States which match an already handled state after resetting the given ignored properties on the target block
+     * to default values will re-use the previously retrieved model
+     *
+     * @param block The block whose models to replace
+     * @param srcBlock The block whose models to re-use
+     * @param ignoredProps The state properties to ignore during copying before applying the target block's properties
+     *                     to the source block for retrieving the wrapped model
+     */
     public static void copy(Holder<Block> block, Holder<Block> srcBlock, Set<Property<?>> ignoredProps)
     {
         copy(block, srcBlock, StateMerger.ignoring(ignoredProps));
     }
 
+    /**
+     * Re-use the wrapped models from the given source block for the given block.
+     * <p>
+     * States which match an already handled state after applying the given {@link StateMerger} will re-use the
+     * previously retrieved model
+     *
+     * @param block The block whose models to replace
+     * @param srcBlock The block whose models to re-use
+     * @param stateMerger The {@link StateMerger} to use for merging visually redundant during copying before applying
+     *                    the target block's properties to the source block for retrieving the wrapped model
+     */
     public static void copy(Holder<Block> block, Holder<Block> srcBlock, StateMerger stateMerger)
     {
         InternalClientAPI.INSTANCE.registerCopyingModelWrapper(block, srcBlock, stateMerger);
