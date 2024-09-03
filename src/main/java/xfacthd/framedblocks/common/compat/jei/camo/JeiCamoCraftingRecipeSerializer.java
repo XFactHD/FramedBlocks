44a8fe7f17b0d1ca7c6046496c7c7e5c1bf28fb5
mezz <tehgeek@gmail.com>
2024-09-03 10:14:20 +0900

    use a proper stream codec

--- src/main/java/xfacthd/framedblocks/common/compat/jei/camo/JeiCamoCraftingRecipeSerializer.java
+++ src/main/java/xfacthd/framedblocks/common/compat/jei/camo/JeiCamoCraftingRecipeSerializer.java
@@ -9,8 +9,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
-import java.util.List;
-
 public final class JeiCamoCraftingRecipeSerializer implements RecipeSerializer<JeiCamoCraftingRecipe>
 {
     private static final MapCodec<JeiCamoCraftingRecipe> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
@@ -20,7 +18,20 @@ public final class JeiCamoCraftingRecipeSerializer implements RecipeSerializer<J
             Ingredient.CODEC.fieldOf("second_ingredient").forGetter(JeiCamoCraftingRecipe::getSecondIngredient),
             Codec.list(ItemStack.CODEC).fieldOf("results").forGetter(JeiCamoCraftingRecipe::getResults)
     ).apply(inst, JeiCamoCraftingRecipe::new));
-    public static final StreamCodec<RegistryFriendlyByteBuf, JeiCamoCraftingRecipe> STREAM_CODEC = StreamCodec.of(JeiCamoCraftingRecipeSerializer::toNetwork, JeiCamoCraftingRecipeSerializer::fromNetwork);
+
+    public static final StreamCodec<RegistryFriendlyByteBuf, JeiCamoCraftingRecipe> STREAM_CODEC = StreamCodec.composite(
+            Ingredient.CONTENTS_STREAM_CODEC,
+            JeiCamoCraftingRecipe::getFrameStacks,
+            Ingredient.CONTENTS_STREAM_CODEC,
+            JeiCamoCraftingRecipe::getCopyTool,
+            Ingredient.CONTENTS_STREAM_CODEC,
+            JeiCamoCraftingRecipe::getFirstIngredient,
+            Ingredient.CONTENTS_STREAM_CODEC,
+            JeiCamoCraftingRecipe::getSecondIngredient,
+            ItemStack.LIST_STREAM_CODEC,
+            JeiCamoCraftingRecipe::getResults,
+            JeiCamoCraftingRecipe::new
+    );
 
     @Override
     public MapCodec<JeiCamoCraftingRecipe> codec()
@@ -32,24 +43,5 @@ public final class JeiCamoCraftingRecipeSerializer implements RecipeSerializer<J
     public StreamCodec<RegistryFriendlyByteBuf, JeiCamoCraftingRecipe> streamCodec()
     {
         return STREAM_CODEC;
-    }
-
-    private static JeiCamoCraftingRecipe fromNetwork(RegistryFriendlyByteBuf buffer)
-    {
-        Ingredient frameStacks = Ingredient.CONTENTS_STREAM_CODEC.decode(buffer);
-        Ingredient copyTool = Ingredient.CONTENTS_STREAM_CODEC.decode(buffer);
-        Ingredient firstIngredient = Ingredient.CONTENTS_STREAM_CODEC.decode(buffer);
-        Ingredient secondIngredient = Ingredient.CONTENTS_STREAM_CODEC.decode(buffer);
-        List<ItemStack> results = ItemStack.LIST_STREAM_CODEC.decode(buffer);
-        return new JeiCamoCraftingRecipe(frameStacks, copyTool, firstIngredient, secondIngredient, results);
-    }
-
-    private static void toNetwork(RegistryFriendlyByteBuf buffer, JeiCamoCraftingRecipe recipe)
-    {
-        Ingredient.CONTENTS_STREAM_CODEC.encode(buffer, recipe.getFrameStacks());
-        Ingredient.CONTENTS_STREAM_CODEC.encode(buffer, recipe.getCopyTool());
-        Ingredient.CONTENTS_STREAM_CODEC.encode(buffer, recipe.getFirstIngredient());
-        Ingredient.CONTENTS_STREAM_CODEC.encode(buffer, recipe.getSecondIngredient());
-        ItemStack.LIST_STREAM_CODEC.encode(buffer, recipe.getResults());
     }
 }
