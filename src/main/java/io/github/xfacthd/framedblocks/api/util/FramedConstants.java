46e6550a8c214698495ab19d0dd9e8a470eb1cc9
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-02-08 00:13:32 +0100

    Clean up registry IDs in FramedConstants

--- src/main/java/io/github/xfacthd/framedblocks/api/util/FramedConstants.java
+++ src/main/java/io/github/xfacthd/framedblocks/api/util/FramedConstants.java
@@ -3,13 +3,16 @@ package io.github.xfacthd.framedblocks.api.util;
 import io.github.xfacthd.framedblocks.api.camo.CamoContainerFactory;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.Identifier;
 
 public final class FramedConstants
 {
     public static final String MOD_ID = "framedblocks";
-    public static final Identifier CAMO_CONTAINER_FACTORY_REGISTRY_NAME = Utils.id("camo_containers");
-    public static final ResourceKey<Registry<CamoContainerFactory<?>>> CAMO_CONTAINER_FACTORY_REGISTRY_KEY = ResourceKey.createRegistryKey(CAMO_CONTAINER_FACTORY_REGISTRY_NAME);
+    public static final ResourceKey<Registry<CamoContainerFactory<?>>> CAMO_CONTAINER_FACTORY_REGISTRY_KEY = registry("camo_container");
+
+    private static <T> ResourceKey<Registry<T>> registry(String name)
+    {
+        return ResourceKey.createRegistryKey(Utils.id(name));
+    }
 
     private FramedConstants() { }
 }
