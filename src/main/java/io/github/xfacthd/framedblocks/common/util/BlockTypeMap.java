a55f613324b36dcca2a7291723a40851b94f6f22
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-02-15 00:10:47 +0100

    Add automatic defaulting and overwrite protection to BlockTypeMap

--- src/main/java/io/github/xfacthd/framedblocks/common/util/BlockTypeMap.java
+++ src/main/java/io/github/xfacthd/framedblocks/common/util/BlockTypeMap.java
@@ -7,6 +7,7 @@ import org.jspecify.annotations.Nullable;
 
 import java.util.Objects;
 import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 
 public abstract class BlockTypeMap<T>
 {
@@ -14,15 +15,33 @@ public abstract class BlockTypeMap<T>
     private static final int TYPE_COUNT = TYPES.length;
 
     private final T defaultValue;
+    @Nullable
+    private final Predicate<BlockType> setDefault;
     private final @Nullable Object[] values = new Object[TYPE_COUNT];
 
     protected BlockTypeMap(T defaultValue)
     {
+        this(defaultValue, null);
+    }
+
+    protected BlockTypeMap(T defaultValue, @Nullable Predicate<BlockType> setDefault)
+    {
         this.defaultValue = defaultValue;
+        this.setDefault = setDefault;
     }
 
     public final void initialize()
     {
+        if (setDefault != null)
+        {
+            for (BlockType type : TYPES)
+            {
+                if (setDefault.test(type))
+                {
+                    put(type, defaultValue);
+                }
+            }
+        }
         fill();
         if (!Utils.PRODUCTION)
         {
@@ -53,6 +72,16 @@ public abstract class BlockTypeMap<T>
 
     protected final void put(BlockType type, T value)
     {
+        if (!Utils.PRODUCTION && values[type.ordinal()] != null)
+        {
+            throw new IllegalStateException(String.format(
+                    "Tried to overwrite mapping assigned to '%s' in '%s' (old: %s, new: %s)",
+                    type,
+                    getClass().getSimpleName(),
+                    values[type.ordinal()],
+                    value
+            ));
+        }
         values[type.ordinal()] = Objects.requireNonNull(value);
     }
 
