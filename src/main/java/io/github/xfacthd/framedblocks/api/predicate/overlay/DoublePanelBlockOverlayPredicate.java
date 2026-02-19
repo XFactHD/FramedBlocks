96c7a3fc0baa8debeb3aab09867fc968737797d4
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-02-19 01:49:45 +0100

    Add missing BlockOverlayPredicate default implementations

---
+++ src/main/java/io/github/xfacthd/framedblocks/api/predicate/overlay/DoublePanelBlockOverlayPredicate.java
@@ -0,0 +1,30 @@
+package io.github.xfacthd.framedblocks.api.predicate.overlay;
+
+import io.github.xfacthd.framedblocks.api.block.FramedProperties;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
+
+final class DoublePanelBlockOverlayPredicate implements BlockOverlayPredicate
+{
+    @Override
+    public boolean supportsSolid(BlockState state, Direction side, boolean secondPart)
+    {
+        Direction facing = state.getValue(FramedProperties.FACING_HOR);
+        if (side == facing) return !secondPart;
+        if (side == facing.getOpposite()) return secondPart;
+        return true;
+    }
+
+    @Override
+    public boolean supportsEdge(BlockState state, Direction side, Direction edge, boolean secondPart, boolean nullCullFace, boolean unaligned)
+    {
+        Direction facing = state.getValue(FramedProperties.FACING_HOR);
+        if (side.getAxis() != facing.getAxis())
+        {
+            if (edge == facing) return !secondPart;
+            if (edge == facing.getOpposite()) return secondPart;
+            return true;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/io/github/xfacthd/framedblocks/api/predicate/overlay/DoubleSlabBlockOverlayPredicate.java
@@ -0,0 +1,34 @@
+package io.github.xfacthd.framedblocks.api.predicate.overlay;
+
+import io.github.xfacthd.framedblocks.api.util.Utils;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
+
+final class DoubleSlabBlockOverlayPredicate implements BlockOverlayPredicate
+{
+    @Override
+    public boolean supportsSolid(BlockState state, Direction side, boolean secondPart)
+    {
+        return switch (side)
+        {
+            case UP -> secondPart;
+            case DOWN -> !secondPart;
+            default -> true;
+        };
+    }
+
+    @Override
+    public boolean supportsEdge(BlockState state, Direction side, Direction edge, boolean secondPart, boolean nullCullFace, boolean unaligned)
+    {
+        if (!Utils.isY(side))
+        {
+            return switch (edge)
+            {
+                case UP -> secondPart;
+                case DOWN -> !secondPart;
+                default -> true;
+            };
+        }
+        return true;
+    }
+}
