6245e3911ef608d0092cc19bab76181c7fb0f95c
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-04-14 23:24:46 +0200

    Prevent unnecessary cache cleanup while joining SP/MP or closing the game from main menu

--- src/main/java/io/github/xfacthd/framedblocks/client/util/ClientEventHandler.java
+++ src/main/java/io/github/xfacthd/framedblocks/client/util/ClientEventHandler.java
@@ -5,7 +5,12 @@ import io.github.xfacthd.framedblocks.common.data.dynreg.BlockOverlayCache;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 
 public final class ClientEventHandler {
-    public static void onClientDisconnect(@SuppressWarnings("unused") ClientPlayerNetworkEvent.LoggingOut event) {
+    public static void onClientDisconnect(ClientPlayerNetworkEvent.LoggingOut event) {
+        if (event.getConnection() == null) {
+            // For some reason, vanilla performs a disconnect before joining an SP world or a server or closing the game
+            return;
+        }
+
         FramingSawRecipeCache.get(true).clear();
         BlockOverlayCache.get(true).clear();
         CacheCleaner.clearModelCaches(CacheCleaner.Reason.DISCONNECT);
