4e07727eae673f08315552d921d364f001e26dc0
XFactHD <11262040+XFactHD@users.noreply.github.com>
2026-07-19 22:27:37 +0200

    Simplify Collapsible Copycat Block geometry

--- src/main/java/io/github/xfacthd/framedblocks/client/model/geometry/cube/FramedCollapsibleCopycatBlockGeometry.java
+++ src/main/java/io/github/xfacthd/framedblocks/client/model/geometry/cube/FramedCollapsibleCopycatBlockGeometry.java
@@ -15,22 +15,18 @@ import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.client.resources.model.geometry.BakedQuad;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.model.data.ModelData;
 import org.jspecify.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class FramedCollapsibleCopycatBlockGeometry extends Geometry {
     public static final String ALT_BASE_MODEL_KEY = "alt_base";
-    private static final int UP = Direction.UP.ordinal();
-    private static final int DOWN = Direction.DOWN.ordinal();
-    private static final int NORTH = Direction.NORTH.ordinal();
-    private static final int EAST = Direction.EAST.ordinal();
-    private static final int SOUTH = Direction.SOUTH.ordinal();
-    private static final int WEST = Direction.WEST.ordinal();
 
     private final BlockState state;
     private final int solidFaces;
@@ -54,109 +50,48 @@ public class FramedCollapsibleCopycatBlockGeometry extends Geometry {
         byte[] offsets = FramedCollapsibleCopycatBlockEntity.unpackOffsets(packedOffsets);
         boolean solid = (solidFaces & (1 << quadDir.ordinal())) != 0;
         List<QuadModifier> mods = new ArrayList<>(2);
-        QuadModifier initialModifier = QuadModifier.of(quad).apply(Modifiers.setPosition((16F - offsets[quadDir.ordinal()]) / 16F));
-        if (DirUtils.isY(quadDir)) {
-            if (offsets[NORTH] > 0 || offsets[SOUTH] > 0) {
-                FloatPair length = getLengths(offsets[NORTH], offsets[SOUTH]);
-                if (length.valOne > 0F) {
-                    mods.add(initialModifier
-                            .derive()
-                            .apply(Modifiers.cut(Direction.SOUTH, length.valOne))
-                            .apply(Modifiers.offset(Direction.SOUTH, offsets[NORTH] / 16F))
-                    );
-                }
-                if (length.valTwo > 0F) {
-                    mods.add(initialModifier
-                            .apply(Modifiers.cut(Direction.NORTH, length.valTwo))
-                            .apply(Modifiers.offset(Direction.NORTH, offsets[SOUTH] / 16F))
-                    );
-                } else {
-                    initialModifier.discard();
-                }
-            } else {
-                mods.add(initialModifier);
-            }
 
-            if (offsets[EAST] > 0 || offsets[WEST] > 0) {
-                FloatPair length = getLengths(offsets[WEST], offsets[EAST]);
-                for (QuadModifier modifier : mods) {
-                    if (length.valOne > 0F) {
-                        modifier.derive()
-                                .apply(Modifiers.cut(Direction.EAST, length.valOne))
-                                .apply(Modifiers.offset(Direction.EAST, offsets[WEST] / 16F))
-                                .export(quadMap, solid ? quadDir : null);
-                    }
-                    if (length.valTwo > 0F) {
-                        modifier.apply(Modifiers.cut(Direction.WEST, length.valTwo))
-                                .apply(Modifiers.offset(Direction.WEST, offsets[EAST] / 16F))
-                                .export(quadMap, solid ? quadDir : null);
-                    } else {
-                        modifier.discard();
-                    }
-                }
-            } else {
-                for (QuadModifier modifier : mods) {
-                    modifier.export(quadMap, solid ? quadDir : null);
-                }
-            }
-        } else {
-            boolean xAxis = DirUtils.isX(quadDir);
-            Direction axisNeg = xAxis ? Direction.NORTH : Direction.WEST;
-            int axisMin = xAxis ? NORTH : WEST;
-            int axisMax = xAxis ? SOUTH : EAST;
-            if (offsets[axisMin] > 0 || offsets[axisMax] > 0) {
-                FloatPair length = getLengths(offsets[axisMin], offsets[axisMax]);
-                if (length.valOne > 0F) {
-                    mods.add(initialModifier
-                            .derive()
-                            .apply(Modifiers.cut(axisNeg.getOpposite(), length.valOne))
-                            .apply(Modifiers.offset(axisNeg.getOpposite(), offsets[axisMin] / 16F))
-                    );
-                }
-                if (length.valTwo > 0F) {
-                    mods.add(initialModifier
-                            .apply(Modifiers.cut(axisNeg, length.valTwo))
-                            .apply(Modifiers.offset(axisNeg, offsets[axisMax] / 16F))
-                    );
-                } else {
-                    initialModifier.discard();
-                }
-            } else {
-                mods.add(initialModifier);
-            }
+        Iterator<Direction.Axis> axes = DirUtils.getPerpendicularAxes(quadDir.getAxis()).iterator();
+        Direction.Axis axisOne = axes.next();
+        Direction.Axis axisTwo = axes.next();
 
-            if (offsets[DOWN] > 0 || offsets[UP] > 0) {
-                FloatPair length = getLengths(offsets[DOWN], offsets[UP]);
-                for (QuadModifier modifier : mods) {
-                    if (length.valOne > 0F) {
-                        modifier.derive()
-                                .apply(Modifiers.cut(Direction.UP, length.valOne))
-                                .apply(Modifiers.offset(Direction.UP, offsets[DOWN] / 16F))
-                                .export(quadMap, solid ? quadDir : null);
-                    }
-                    if (length.valTwo > 0F) {
-                        modifier.apply(Modifiers.cut(Direction.DOWN, length.valTwo))
-                                .apply(Modifiers.offset(Direction.DOWN, offsets[UP] / 16F))
-                                .export(quadMap, solid ? quadDir : null);
-                    } else {
-                        modifier.discard();
-                    }
-                }
-            } else {
-                for (QuadModifier modifier : mods) {
-                    modifier.export(quadMap, solid ? quadDir : null);
-                }
-            }
+        float offOneNeg = offsets[axisOne.getNegative().ordinal()] / 16F;
+        float offOnePos = offsets[axisOne.getPositive().ordinal()] / 16F;
+        float offTwoNeg = offsets[axisTwo.getNegative().ordinal()] / 16F;
+        float offTwoPos = offsets[axisTwo.getPositive().ordinal()] / 16F;
+        byte depthOff = offsets[quadDir.ordinal()];
+
+        boolean fullOne = Mth.equal(offOneNeg, 0F) && Mth.equal(offOnePos, 0F);
+        boolean fullTwo = Mth.equal(offTwoNeg, 0F) && Mth.equal(offTwoPos, 0F);
+
+        QuadModifier baseModifier = QuadModifier.of(quad);
+        if (depthOff > 0) {
+            baseModifier = baseModifier.apply(Modifiers.setPosition((16F - depthOff) / 16F));
         }
-    }
+        if (fullOne && fullTwo) {
+            if (depthOff == 0) {
+                baseModifier.discard();
+                return;
+            }
 
-    private static FloatPair getLengths(int offsetMin, int offsetMax) {
-        float length = (16 - offsetMin - offsetMax) / 2F;
-        boolean ceilFirst = offsetMin > offsetMax;
-        return new FloatPair(
-                (float)(ceilFirst ? Math.ceil(length) : Math.floor(length)) / 16F,
-                (float)(ceilFirst ? Math.floor(length) : Math.ceil(length)) / 16F
-        );
+            mods.add(baseModifier);
+        } else if (fullOne) {
+            mods.add(baseModifier.derive().apply(Modifiers.cutCopycat(axisTwo.getNegative(), offTwoNeg, offTwoPos)));
+            mods.add(baseModifier         .apply(Modifiers.cutCopycat(axisTwo.getPositive(), offTwoNeg, offTwoPos)));
+        } else if (fullTwo) {
+            mods.add(baseModifier.derive().apply(Modifiers.cutCopycat(axisOne.getNegative(), offOneNeg, offOnePos)));
+            mods.add(baseModifier         .apply(Modifiers.cutCopycat(axisOne.getPositive(), offOneNeg, offOnePos)));
+        } else {
+            mods.add(baseModifier.derive().apply(Modifiers.cutCopycat(axisOne.getNegative(), axisTwo.getNegative(), offOneNeg, offOnePos, offTwoNeg, offTwoPos)));
+            mods.add(baseModifier.derive().apply(Modifiers.cutCopycat(axisOne.getNegative(), axisTwo.getPositive(), offOneNeg, offOnePos, offTwoNeg, offTwoPos)));
+            mods.add(baseModifier.derive().apply(Modifiers.cutCopycat(axisOne.getPositive(), axisTwo.getNegative(), offOneNeg, offOnePos, offTwoNeg, offTwoPos)));
+            mods.add(baseModifier         .apply(Modifiers.cutCopycat(axisOne.getPositive(), axisTwo.getPositive(), offOneNeg, offOnePos, offTwoNeg, offTwoPos)));
+        }
+
+        Direction cullFace = solid ? quadDir : null;
+        for (QuadModifier modifier : mods) {
+            modifier.export(quadMap, cullFace);
+        }
     }
 
     @Override
@@ -173,6 +108,4 @@ public class FramedCollapsibleCopycatBlockGeometry extends Geometry {
     public @Nullable Object computeCacheKeyUserData(BlockAndTintGetter level, BlockPos pos, RandomSource random, ModelData data) {
         return data.get(PackedCollapsibleBlockOffsets.PROPERTY);
     }
-
-    private record FloatPair(float valOne, float valTwo) { }
 }
